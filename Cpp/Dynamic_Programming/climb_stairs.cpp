58d8895dbf7b0c475ee2ccce503e5bf94a6d5931
VikasSingh7221 <vikassingh15150109@gmail.com>
2025-12-28 08:13:21 +0000

    cleaning repo

--- Cpp/Dynamic_Programming/climb_stairs.cpp
+++
@@ -1,6 +0,0 @@
-// Given an integer n, there is a staircase with n steps, starting from the 0th step.
-// Determine the number of unique ways to reach the nth step, 
-// given that each move can be either 1 or 2 steps at a time.
-
-
-
